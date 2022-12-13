c0fe4fd064e112b0066d2513e1a1fdf89b0cba28
Habibur Rahman <habib97.cs@gmail.com>
2022-12-13 17:01:50 +0600

    added some user in json file and getting all users and users by limit

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,3 +1,26 @@
-module.exports.getAllUsers = (req, res, next) => {
-    console.log("getting users");
+const fs = require('fs/promises');
+
+module.exports.getAllUsers = async (req, res, next) => {
+    try {
+        // getting limit from query 
+        const limit = req.query.limit;
+
+        // Reading userData.json file 
+        const usersJson = await fs.readFile('./usersData.json');
+        const users = JSON.parse(usersJson);
+
+        // If limit then sending data by limit otherwise sending all data 
+        if (limit) {
+            let limitData = [];
+            for (let i = 0; i < limit; i++) {
+                currentData = users[i];
+                limitData.push(currentData);
+            };
+            res.send(limitData);
+        } else {
+            res.send(users);
+        };
+    } catch (error) {
+        next(error);
+    };
 };
